3ec0f9ba6b8235b2306c7c9273166a494eb3e705
koen-benne <koen.benne@gmail.com>
2022-11-09 14:49:55 +0100

    make things look more epicer

--- dwm/.config/dwm/dwm-autostart.sh
+++ dwm/.config/dwm/dwm-autostart.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 wpa_supplicant -B -i wlp3s0 -c /etc/wpa_supplicant/wpa_supplicant.conf
-feh --bg-scale /home/koen/Images/bg.jpeg
+# feh --bg-scale /home/koen/Images/bg.jpeg
+feh --bg-scale /home/koen/Images/bg3.jpg
 picom -b --experimental-backend --config /home/koen/.config/picom.conf --animations --animation-window-mass 0.5 --animation-for-open-window zoom --animation-stiffness 350
 dbus-launch --sh-syntax --exit-with-session dwm
 spotifyd
