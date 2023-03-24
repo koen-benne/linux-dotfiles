b8e5dc9fab76390a576c9ffa309716c46b45b3ab
koen-benne <koen.benne@gmail.com>
2023-03-24 16:24:12 +0100

    ool

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,24 +11,25 @@ alias l="ls -la"
 alias xi="sudo xbps-install"
 alias xr="sudo xbps-remove"
 alias spt="spotify-tui"
-alias ioslack="/home/koen/.scripts/ioslack.sh & && disown"
-alias lg="lazygit"
-alias nvide="neovide --multigrid"
+alias ioslack=$HOME"/.scripts/ioslack.sh & && disown"
 alias bakkes="WINEESYNC=1 protontricks -c '~/.steam/steam/compatibilitytools.d/GE-Proton7-42/files/bin/wine64 /home/koen/.local/share/Steam/steamapps/compatdata/252950/pfx/drive_c/Program\ Files/BakkesMod/BakkesMod.exe' 252950"
 alias steamui="steam -gamepadui &"
+alias nvide="neovide --multigrid"
 
+# tmux stuff
 alias to="turbotmux.sh open"
 alias tc="turbotmux.sh close"
 alias tp="turbotmux.sh project"
 alias tn="turbotmux.sh new"
+alias lg="lazygit"
 
-fish_add_path $HOME'/.local/bin/'
-fish_add_path $HOME'/.platformsh/bin'
-fish_add_path $HOME'/.cargo/bin'
-fish_add_path $HOME'/.asdf/bin'
+fish_add_path $HOME"/.local/bin/"
+fish_add_path $HOME"/.platformsh/bin"
+fish_add_path $HOME"/.cargo/bin"
+fish_add_path $HOME"/.asdf/bin"
 
 # pnpm
-set -gx PNPM_HOME "/home/koen/.local/share/pnpm"
+set -gx PNPM_HOME $HOME"/.local/share/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
 # pnpm end
 
--- scripts/.local/bin/turbotmux.sh
+++ scripts/.local/bin/turbotmux.sh
@@ -3,49 +3,57 @@
 
 tmux_running=$(pgrep tmux)
 
-if [ $1 = "open" ]; then
-  if [ -z $tmux_running ]; then
-    echo "No tmux sessions running"
-    exit 0
-  fi
-  sesh=$(tmux list-sessions | fzf-tmux | cut -d ":" -f 1)
-  if [ -z "${TMUX}" ]; then
-    tmux attach-session -t ${sesh}
-  else
-    tmux switch-client -t ${sesh}
-  fi
-elif [ $1 = "close" ]; then
-  if [ -z $tmux_running ]; then
-    echo "No tmux sessions running"
-    exit 0
-  elif [ -n $2 ]; then
-    tmux kill-session -t ${2}
-    echo "Session ${2} closed"
-  else
+case $1 in
+  open)
+    if [ -z $tmux_running ]; then
+      echo "No tmux sessions running"
+      exit 0
+    fi
     sesh=$(tmux list-sessions | fzf-tmux | cut -d ":" -f 1)
-    tmux kill-session -t ${sesh}
-    echo "Session ${sesh} closed"
-  fi
-elif [ $1 = "new" ]; then
-  if [ -z $2 ]; then
-    echo "Please provide a name for the new session"
+    if [ -z "${TMUX}" ]; then
+      tmux attach-session -t ${sesh}
+    else
+      tmux switch-client -t ${sesh}
+    fi
+    ;;
+  close)
+    if [ -z $tmux_running ]; then
+      echo "No tmux sessions running"
+      exit 0
+    elif [ -n $2 ]; then
+      tmux kill-session -t ${2}
+      echo "Session ${2} closed"
+    else
+      sesh=$(tmux list-sessions | fzf-tmux | cut -d ":" -f 1)
+      tmux kill-session -t ${sesh}
+      echo "Session ${sesh} closed"
+    fi
+    ;;
+  new)
+    if [ -z $2 ]; then
+      echo "Please provide a name for the new session"
+      exit 1
+    fi
+    tmux new-session -s ${2}
+    ;;
+  project)
+    # project=$(find $proj_dirs -name ".git" -mindepth=1 -maxdepth=1 -type d | sed 's/\/.git//g' | fzf-tmux)
+    project=$(find ~/work/ ~/personal/ -mindepth 1 -maxdepth 1 -type d | fzf-tmux)
+    proj_name=$(basename ${project} | tr . _)
+    echo "Opening ${proj_name} in tmux"
+    tmux new-session -s ${proj_name} -c ${project} -d
+    tmux send-keys -t ${proj_name} "nvim" C-m
+    if [ -z "${TMUX}" ]; then
+      tmux attach-session -t ${proj_name}
+    else
+      tmux switch-client -t ${proj_name}
+    fi
+    ;;
+  *)
+    echo "Usage: tmuxing [open|close|new|project]"
     exit 1
-  fi
-  tmux new-session -s ${2}
-elif [ $1 = "project" ]; then
-  proj_dirs=${HOME}/Documents/coding/ #${HOME}/.dotfiles/
-  # project=$(find $proj_dirs -name ".git" -mindepth=1 -maxdepth=1 -type d | sed 's/\/.git//g' | fzf-tmux)
-  project=$(find $proj_dirs -mindepth 1 -maxdepth 1 -type d | fzf-tmux)
-  proj_name=$(basename ${project})
-  tmux new-session -s ${proj_name} -c ${project} -d
-  tmux send-keys -t ${proj_name} "nvim" C-m
-  if [ -z "${TMUX}" ]; then
-    tmux attach-session -t ${proj_name}
-  else
-    tmux switch-client -t ${proj_name}
-  fi
-else
-  echo "Usage: tmuxing [open|close|new|project|list]"
-  exit 1
-fi
+    ;;
+esac
+
+exit 0
 
