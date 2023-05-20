e4ce32a4312ee9c0b9be56cd0f60d7f5f4051d65
koen-benne <koen.benne@gmail.com>
2023-05-20 16:08:41 +0200

    do tings

--- river/.config/river/autostart.sh
+++ river/.config/river/autostart.sh
@@ -3,6 +3,7 @@ swaybg -i $HOME/Backgrounds/bg4.png &
 bash /usr/libexec/polkit-mate-authentication-agent-1 &
 waybar &
 dunst &
+foot --server &
 dbus-update-activation-environment --all &
 gnome-keyring-daemon --start --components=secrets &
 /usr/libexec/polkit-mate-authentication-agent-1 &
